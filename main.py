527798618015166ec46675ff5d168fad1c8e6cea
Ben Orr <104040007+Ben-J-Orr@users.noreply.github.com>
2023-03-31 00:15:34 +0100

    Update main.py
    The improvements include:
    
    Encapsulating the API call in a function generate_cover_letter for better readability and reusability.
    Using enumerate to loop through the job files and get the count directly, making the code cleaner.
    Using os.path.join to construct file paths, which is more portable and less prone to errors.
    Wrapping the main logic in a main() function and using the if __name__ == "__main__": block to call it, making the code more modular and importable.

--- main.py
+++ main.py
@@ -1,34 +1,50 @@
-# IMPORTS #
+# IMPORTS
 import os
 import openai
 import time
-start_time = time.time()
-# API KEY #
-openai.api_key = "API-KEY"
-count = 1
-# READ RESUME # 
-with open('resume/experience.txt', encoding="utf8") as f:
-    experience = f.read()
 
-# LOOP THROUGH JOBS # 
-for i in os.listdir("Adverts/"): 
-    # READ PERSPECTIVE JOB AD # 
-    print (f"Current Job [{count}]: {i}")
-    with open(f'adverts/{i}') as f:
-        job_ad = f.read()
-
-    # API CALL #
-    content = ("This is the content of my resume: " + experience + "Write a cover letter for the following job advert: " + 
+def generate_cover_letter(experience, job_ad):
+    content = ("This is the content of my resume: " + experience + 
+               "Write a cover letter for the following job advert: " + 
                job_ad + " Keep it short. And make reference to my above resume. Include my name at the end. When referencing the job advert, Dont be too literal or too wordy.")
     completion = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[{"role": "user", "content": content}])
-    chat_response = completion.choices[0].message.content
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": content}]
+    )
+    return completion.choices[0].message.content
 
-    # DUMP OUTPUT TO FILE #
-    with open(f'outputs/{i}_Output.txt', 'w') as file:
-        file.write(chat_response)
-    count = count + 1 
+def main():
+    # CONSTANTS
+    API_KEY = "API-KEY"
+    RESUME_PATH = 'resume/experience.txt'
+    ADVERTS_DIR = "Adverts/"
+    OUTPUT_DIR = "outputs/"
 
-# END MESSAGE + ELAPSED TIME #
-input(f"\nCompleted all jobs [{count-1}] in {round((time.time() - start_time),2)} seconds.")
+    # API KEY
+    openai.api_key = API_KEY
+
+    # READ RESUME
+    with open(RESUME_PATH, encoding="utf8") as f:
+        experience = f.read()
+
+    # LOOP THROUGH JOBS
+    start_time = time.time()
+    for count, filename in enumerate(os.listdir(ADVERTS_DIR), start=1):
+        print(f"Current Job [{count}]: {filename}")
+
+        # READ PERSPECTIVE JOB AD
+        with open(os.path.join(ADVERTS_DIR, filename)) as f:
+            job_ad = f.read()
+
+        # API CALL
+        chat_response = generate_cover_letter(experience, job_ad)
+
+        # DUMP OUTPUT TO FILE
+        with open(os.path.join(OUTPUT_DIR, f'{filename}_Output.txt'), 'w') as file:
+            file.write(chat_response)
+
+    # END MESSAGE + ELAPSED TIME
+    input(f"\nCompleted all jobs [{count}] in {round((time.time() - start_time),2)} seconds.")
+
+if __name__ == "__main__":
+    main()
